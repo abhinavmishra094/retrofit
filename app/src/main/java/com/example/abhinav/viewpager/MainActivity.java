400e2ef33c044339fb76123a59815cc028e9d713
abhinav <abhinavmishra094@gmail.com>
2017-10-18 12:01:34 +0530

    scroll inside text view

--- app/src/main/java/com/example/abhinav/viewpager/MainActivity.java
+++ app/src/main/java/com/example/abhinav/viewpager/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.abhinav.viewpager;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -15,10 +18,13 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     static final String TAG = MainActivity.class.getName();
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text);
+        text.setMovementMethod(new ScrollingMovementMethod());
         ApiService api = RetroClient.getApiService();
         Call<ContactList> call = api.getMyJSON();
         call.enqueue(new Callback<ContactList>() {
@@ -33,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
                     ArrayList<Contact> contactList = response.body().getContacts();
                     try {
                         Log.i(TAG,"contact \n"+new ObjectMapper().writeValueAsString(contactList));
+                        text.setText(new ObjectMapper().writeValueAsString(contactList));
                     } catch (Throwable e) {
                         e.printStackTrace();
                     }
